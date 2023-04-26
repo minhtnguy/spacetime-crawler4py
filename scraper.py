844b9ca3f0518ce81d7e06be41956083a7963183
mtnguy16 <mtnguy16@uci.edu>
2023-04-26 09:23:26 -0700

    added uci ids

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 import re
 from urllib.parse import urlparse
 
+from collections import Counter
+import requests
+from bs4 import BeautifulSoup
+
+
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
